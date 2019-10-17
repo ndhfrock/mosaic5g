5dff3193d69018ffdc2d72046ce18948dae4a583
nadhif <nadhif@nadhif.openair4G.eur>
2019-10-17 20:20:41 +0800

    test

--- pkg/controller/mosaic5g/mosaic5g_controller.go
+++ pkg/controller/mosaic5g/mosaic5g_controller.go
@@ -301,7 +301,7 @@ func (r *ReconcileMosaic5g) genConfigMap(m *mosaic5gv1alpha1.Mosaic5g) *corev1.C
 	cm := corev1.ConfigMap{
 		Data: datas,
 	}
-	cm.Name = "config"
+	cm.Name = "mosaic5g-config"
 	cm.Namespace = m.Namespace
 	genLogger.Info("Done Creating Config Map")
 	return &cm
